4909876b7caa0f8dd36507fed0084399c6ad3334
s3xmaxman <badside3919@gmail.com>
2024-10-13 21:24:12 +0900

    update home.screen.tsx

--- screens/home.screen.tsx
+++ screens/home.screen.tsx
@@ -20,6 +20,13 @@ import Regenerate from "@/assets/svgs/regenerate";
 import Reload from "@/assets/svgs/reload";
 
 const HomeScreen = () => {
+  const [text, setText] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [AIResponse, setAIResponse] = useState(false);
+  const [AISpeaking, setAISpeaking] = useState(false);
+  const lottieRef = useRef<LottieView>(null);
+
   return (
     <LinearGradient
       colors={["#250152", "#000"]}
@@ -27,6 +34,28 @@ const HomeScreen = () => {
       end={{ x: 1, y: 1 }}
       style={styles.container}
     >
+      <StatusBar barStyle="light-content" />
+
+      {/* back shadows */}
+      <Image
+        source={require("@/assets/main/blur.png")}
+        style={{
+          position: "absolute",
+          right: scale(-15),
+          top: 0,
+          width: scale(240),
+        }}
+      />
+      <Image
+        source={require("@/assets/main/purple-blur.png")}
+        style={{
+          position: "absolute",
+          left: scale(-15),
+          bottom: verticalScale(100),
+          width: scale(210),
+        }}
+      />
+
       <Text>HomeScreen</Text>
     </LinearGradient>
   );
